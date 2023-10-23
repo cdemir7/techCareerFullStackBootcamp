4a26920280e06733677bbc4b10cda9dc4eb9eb61
Cihan DEMİR <56312037+cdemir7@users.noreply.github.com>
2023-10-23 10:51:28 +0300

    Math, Cast

--- Frontend/_4_javascript/template.js
+++ Frontend/_4_javascript/template.js
@@ -1,10 +1,10 @@
 // single comment
 /* multiple commment*/
 
-console.log("console");
-console.error("console");
-console.warn("console");
-console.info("console");
+// console.log("console");
+// console.error("console");
+// console.warn("console");
+// console.info("console");
 
 // window.alert("deneme37");
 // alert("deneme37");
@@ -12,51 +12,106 @@ console.info("console");
 
 //////////////////////////////////////////////////////////////////////////////////
 
-var dataValue = 14;
-console.log(dataValue);
+// var dataValue = 14;
+// console.log(dataValue);
 
-var dataValue44 = 44;
-console.log(dataValue44);
+// var dataValue44 = 44;
+// console.log(dataValue44);
 
-var _dataValue44 = 44;
-console.log(_dataValue44);
+// var _dataValue44 = 44;
+// console.log(_dataValue44);
 
-var $_dataValue44 = 44;
-console.log($_dataValue44);
+// var $_dataValue44 = 44;
+// console.log($_dataValue44);
 
 // var `~&444$_dataValue44=44;
 
-var data = 99;
-console.log(typeof data);
+// var data = 99;
+// console.log(typeof data);
 
-var data = "Kelime";
-console.log(typeof data);
+// var data = "Kelime";
+// console.log(typeof data);
 
-var data = 3 > 1;
-console.log(typeof data);
+// var data = 3 > 1;
+// console.log(typeof data);
 
-// hoisting
-data = 56566;
-var data;
-console.log(data);
+// // hoisting
+// data = 56566;
+// var data;
+// console.log(data);
 
 //////////////////////////////////////////////////////////////////////////////////
 
-var globalVariable = "String-1"
-console.log(globalVariable);
+// var globalVariable = "String-1"
+// console.log(globalVariable);
 
-var globalVariable = "String-2"
-console.log(globalVariable);
+// var globalVariable = "String-2"
+// console.log(globalVariable);
 
-let localVariable = true;
-localVariable = false;
+// let localVariable = true;
+// localVariable = false;
 
-const localVariable1 = true;
+// const localVariable1 = true;
 
 //////////////////////////////////////////////////////////////////////////////////
 // Operatorler
- // + - * / % ! &&=VE ||=VEYA
- // x++ ,  ++x , x-- , --x
- // x++;
- // x=x+1;
- // x+=1;
+// + - * / % ! &&=VE ||=VEYA
+// x++ ,  ++x , x-- , --x
+// x++;
+// x=x+1;
+// x+=1;
+
+//////////////////////////////////////////////////////////////////////////////////
+// let userData=prompt("Lütfen adınızı giriniz "); //Kullanıcıdan girdi alma
+// console.log(userData);
+
+//Infinity
+console.log(4/0);
+
+//NaN
+console.log(4/"asd");
+
+//Undefined
+let data;
+console.log(data);
+
+//////////////////////////////////////////////////////////////////////////////////
+// Cast
+console.log(4+4);
+console.log(4+ Number("4"));
+console.log(4+ parseInt("4"));
+console.log(4+ String(4));
+
+// Math
+console.log(Math.min(2,1,-500,6,900));
+console.log(Math.max(2,1,-500,6,900));
+console.log(Math.abs(-500));
+console.log(Math.sqrt(16));
+console.log(Math.pow(2,5));
+
+console.log(Math.floor(2.9));
+console.log(Math.ceil(2.1));
+console.log(Math.round(5.4));
+console.log(Math.round(5.5));
+
+console.log((Math.random()*9+1).toFixed(3));
+console.log(Math.floor((Math.random()*9+1).toFixed(3)) );
+
+console.log(Math.sin(-1));
+
+///////////////////////////////////////////////////////////////// 
+// Function 
+
+// Conditional
+
+// Loop 
+
+// Dizi 
+
+// Object 
+
+// DOM 
+////////////////////////////////////////////////////////////////////
+// Form input Handling && jQuery
+
+// callback, promise, asyn-await
