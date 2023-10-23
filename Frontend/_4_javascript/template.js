158123d20e3c04cc061df30dcadd7f8b6844ede8
Cihan DEMİR <56312037+cdemir7@users.noreply.github.com>
2023-10-23 10:31:24 +0300

    JS

---
+++ Frontend/_4_javascript/template.js
@@ -0,0 +1,62 @@
+// single comment
+/* multiple commment*/
+
+console.log("console");
+console.error("console");
+console.warn("console");
+console.info("console");
+
+// window.alert("deneme37");
+// alert("deneme37");
+
+
+//////////////////////////////////////////////////////////////////////////////////
+
+var dataValue = 14;
+console.log(dataValue);
+
+var dataValue44 = 44;
+console.log(dataValue44);
+
+var _dataValue44 = 44;
+console.log(_dataValue44);
+
+var $_dataValue44 = 44;
+console.log($_dataValue44);
+
+// var `~&444$_dataValue44=44;
+
+var data = 99;
+console.log(typeof data);
+
+var data = "Kelime";
+console.log(typeof data);
+
+var data = 3 > 1;
+console.log(typeof data);
+
+// hoisting
+data = 56566;
+var data;
+console.log(data);
+
+//////////////////////////////////////////////////////////////////////////////////
+
+var globalVariable = "String-1"
+console.log(globalVariable);
+
+var globalVariable = "String-2"
+console.log(globalVariable);
+
+let localVariable = true;
+localVariable = false;
+
+const localVariable1 = true;
+
+//////////////////////////////////////////////////////////////////////////////////
+// Operatorler
+ // + - * / % ! &&=VE ||=VEYA
+ // x++ ,  ++x , x-- , --x
+ // x++;
+ // x=x+1;
+ // x+=1;
